d2b8076e4db2a7a7408c8da8636a293ca6110e31
suhailmhd070 <88363214+suhailmhd070@users.noreply.github.com>
2021-10-18 09:30:11 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 500,
+    delay: (el, i) => 50 * (i+1)
+  })
